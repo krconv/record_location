6d7b265632f4310fe7ab86f50f737fc55f3a25e6
Kodey Converse <kconverse@128technology.com>
2019-02-23 13:39:31 -0500

    Create a client for writing data points to Influx

--- record_location/config.example.py
+++ record_location/config.example.py
@@ -1,3 +1,10 @@
 class Life360:
     base_url = "https://www.life360.com/v3/"
     auth = "Bearer super_secret_token"
+
+class InfluxDB:
+    host = "influx.example.com"
+    port = 8086
+    username = "username"
+    password = "password"
+    database = "database"
